2c90bea84b9414409317f0a0dcafe0c122e51dfa
= <1216936151@qq.com>
2017-12-12 15:42:03 +0800

    SVM 算法

---
+++ statistics/classify/SVM.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+import numpy as np
+import math
+
+
+class SVM:
+
+    def __init__(self):
+        print("init svm")
+
+    def simple_train_set(self):
+        train = np.array(
+            [1]
+        )
+
+        return train
+
+    #SMO算法实现
+    def evel(self):
+
+        print("evel")
+
+if __name__ == '__main__':
+
+    svm = SVM()
+
+    svm.evel()
+
+
